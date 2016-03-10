70fd49d0b49bcad1404fd45ca30f37d11cc183a4
Charles Singleton <charles.singleton+git@gmail.com>
2016-03-10 17:08:32 -0500

    testing the SDL window

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <SDL/SDL.h>
 
 int main (int argc, char * argv[])
 {
-  std::cout << "Helo wurld" << std::endl;
+  std::cout << "starting..." << std::endl;
+
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  SDL_Delay(10000);
+
+  SDL_Quit();
 }
